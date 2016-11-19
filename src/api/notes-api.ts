e7edbbbc81e34c70f5595f80420c0203bf0843dc
Mikko Vuorinen <vuorinem@gmail.com>
2016-11-19 19:57:40 +0000

    Implement requesting notes from Api
    Add request configuration in NotesApi
    Implement getNotes
    Add apiUrl in dev environment configuration

--- src/api/notes-api.ts
+++ src/api/notes-api.ts
@@ -1,10 +1,24 @@
 import { autoinject } from "aurelia-framework";
-import { HttpClient, json } from "aurelia-fetch-client";
+import { HttpClient } from "aurelia-fetch-client";
+import environment from '../environment';
 
 @autoinject
 export class NotesApi {
 
     constructor(private httpClient: HttpClient) {
+        this.httpClient.configure(config => config
+            .withBaseUrl(environment.apiUrl)
+            .withDefaults({
+                headers: {
+                    'Accept': 'application/json',
+                }
+            })
+        );
+    }
+
+    public getNotes(): Promise<Array<any>> {
+        return this.httpClient.fetch('/notes')
+            .then(response => response.json())
     }
 
 }
