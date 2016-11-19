b7b64cdbc3c8c3eef57f134ec7f8b9bf6ed95b2e
Mikko Vuorinen <vuorinem@gmail.com>
2016-11-19 19:59:56 +0000

    Load nodes into front page
    Use NotesApi in App to load notes into front page
    Show all loaded notes in the front page

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+import { autoinject } from 'aurelia-framework';
+import { NotesApi } from './api/notes-api';
+
+@autoinject
 export class App {
-  message = 'Hello World!';
+
+  private notes: Array<string> = [];
+
+  constructor(private notesApi: NotesApi) {
+  }
+
+  private attached() {
+    this.notesApi.getNotes()
+      .then(notes => this.notes.push(...notes));
+  }
+
 }
